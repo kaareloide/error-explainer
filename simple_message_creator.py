a71df0597997c980e0b954819bdd919b56fe37ac
kaarel <kaarel.loide@gmail.com>
2020-01-31 14:33:56 +0200

    clean up simple_message_creator.py

--- simple_message_creator.py
+++ simple_message_creator.py
@@ -11,9 +11,54 @@ class SimpleMessageCreator(object):
         self.message_provider = MessageProvider()
         self.message_provider.parse_messages()
         self.messages = []
+        self.missing_brackets_res = None
 
     def add_message(self, error, code, **namespace):
         self.messages.append((error, self.message_provider.get_formatted_message(code, **namespace)))
+
+    def run_check_invalid_function_def(self, error, path, line_num):
+        if check_invalid_function_def(error):
+            invalid_function_name_res = check_invalid_function_name(
+                utils.get_lines(path,[get_line_location(error)])[0])
+            print(invalid_function_name_res)
+            if invalid_function_name_res is not None:
+                self.add_message(error, "invalid_function_name",
+                                 line=line_num, invalid_name=invalid_function_name_res)
+
+    def run_missing_brackets_checks(self, error, path, line_num):
+        self.missing_brackets_res = check_missing_brackets(error)
+        missing_normal_brackets_res = self.missing_brackets_res[0]
+        missing_square_brackets_res = self.missing_brackets_res[1]
+        missing_curly_brackets_res = self.missing_brackets_res[2]
+
+        if sum(self.missing_brackets_res) != 0:
+            # If there are bracket errors check for miss matched brackets
+            miss_matched_bracket_type_res = check_miss_matched_bracket_type(path)
+            if miss_matched_bracket_type_res == 1:
+                self.add_message(error, "miss_matched_brackets.square.normal", line=line_num)
+            elif miss_matched_bracket_type_res == 2:
+                self.add_message(error, "miss_matched_brackets.curly.normal", line=line_num)
+            elif miss_matched_bracket_type_res == 3:
+                self.add_message(error, "miss_matched_brackets.curly.square", line=line_num)
+            else:
+                if missing_normal_brackets_res < 0:
+                    self.add_message(error, "missing_brackets.normal.opening",
+                                     count=abs(missing_normal_brackets_res), line=line_num)
+                elif missing_normal_brackets_res > 0:
+                    self.add_message(error, "missing_brackets.normal.closing",
+                                     count=abs(missing_normal_brackets_res), line=line_num)
+                elif missing_square_brackets_res < 0:
+                    self.add_message(error, "missing_brackets.square.opening",
+                                     count=abs(missing_square_brackets_res), line=line_num)
+                elif missing_square_brackets_res > 0:
+                    self.add_message(error, "missing_brackets.square.closing",
+                                     count=abs(missing_square_brackets_res), line=line_num)
+                elif missing_curly_brackets_res < 0:
+                    self.add_message(error, "missing_brackets.curly.opening",
+                                     count=abs(missing_curly_brackets_res), line=line_num)
+                elif missing_curly_brackets_res > 0:
+                    self.add_message(error, "missing_brackets.curly.closing",
+                                     count=abs(missing_curly_brackets_res), line=line_num)
 
     def get_simple_error_messages(self, root_node, path):
         """
@@ -24,51 +69,13 @@ class SimpleMessageCreator(object):
         """
         found_errors = find_nodes_of_type(root_node, parso.python.tree.PythonErrorNode)
         for error in found_errors:
-            missing_colon_result = None
             line_num = get_line_location(error)
-            if check_invalid_function_def(error):
-                invalid_function_name_res = check_invalid_function_name(utils.get_lines(path,
-                                                                                        [get_line_location(error)])[0])
-                print(invalid_function_name_res)
-                if invalid_function_name_res is not None:
-                    self.add_message(error, "invalid_function_name",
-                                     line=line_num, invalid_name=invalid_function_name_res)
+            missing_colon_result = None
+            self.run_check_invalid_function_def(error, path, line_num)
 
-            # All error checks go here
-            missing_brackets_res = check_missing_brackets(error)
-            missing_normal_brackets_res = missing_brackets_res[0]
-            missing_square_brackets_res = missing_brackets_res[1]
-            missing_curly_brackets_res = missing_brackets_res[2]
+            self.run_missing_brackets_checks(error, path, line_num)
 
-            if sum(missing_brackets_res) != 0:
-                # If there are bracket errors check for miss matched brackets
-                miss_matched_bracket_type_res = check_miss_matched_bracket_type(path)
-                if miss_matched_bracket_type_res == 1:
-                    self.add_message(error, "miss_matched_brackets.square.normal", line=line_num)
-                elif miss_matched_bracket_type_res == 2:
-                    self.add_message(error, "miss_matched_brackets.curly.normal", line=line_num)
-                elif miss_matched_bracket_type_res == 3:
-                    self.add_message(error, "miss_matched_brackets.curly.square", line=line_num)
-                else:
-                    if missing_normal_brackets_res < 0:
-                        self.add_message(error, "missing_brackets.normal.opening",
-                                         count=abs(missing_normal_brackets_res), line=line_num)
-                    elif missing_normal_brackets_res > 0:
-                        self.add_message(error, "missing_brackets.normal.closing",
-                                         count=abs(missing_normal_brackets_res), line=line_num)
-                    elif missing_square_brackets_res < 0:
-                        self.add_message(error, "missing_brackets.square.opening",
-                                         count=abs(missing_square_brackets_res), line=line_num)
-                    elif missing_square_brackets_res > 0:
-                        self.add_message(error, "missing_brackets.square.closing",
-                                         count=abs(missing_square_brackets_res), line=line_num)
-                    elif missing_curly_brackets_res < 0:
-                        self.add_message(error, "missing_brackets.curly.opening",
-                                         count=abs(missing_curly_brackets_res), line=line_num)
-                    elif missing_curly_brackets_res > 0:
-                        self.add_message(error, "missing_brackets.curly.closing",
-                                         count=abs(missing_curly_brackets_res), line=line_num)
-            else:
+            if sum(self.missing_brackets_res) == 0:
                 missing_colon_result = check_missing_colon(error)
 
             if check_print_missing_brackets(error):
