5e0d982ef5f45b45ae798cb42b84e17ff1dbc2ae
kaarel <kaarel.loide@gmail.com>
2020-04-05 14:17:16 +0300

    fix __init__

--- __init__.py
+++
@@ -1,6 +0,0 @@
-from error_explainer.check_runner import *
-from error_explainer.utils import *
-from error_explainer.messages import *
-from error_explainer.colon_statements import *
-from error_explainer.search_utils import *
-from error_explainer.checks import *
---
+++ error_explainer/__init__.py
@@ -0,0 +1,6 @@
+from error_explainer.check_runner import *
+from error_explainer.utils import *
+from error_explainer.messages import *
+from error_explainer.colon_statements import *
+from error_explainer.search_utils import *
+from error_explainer.checks import *
